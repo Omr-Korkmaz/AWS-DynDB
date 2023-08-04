451ad7b0208bdb5d2e53406057b4ad62d1353cb5
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-08-04 23:07:37 +0200

    add react router dom and create paths

--- src/components/comman/SiteNav.js
+++ src/components/comman/SiteNav.js
@@ -12,8 +12,8 @@ const SiteNav = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">login</Nav.Link>
+            <Nav.Link href="#link">Register</Nav.Link>
            
           </Nav>
         </Navbar.Collapse>
