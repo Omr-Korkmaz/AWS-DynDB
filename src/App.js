d4b85ca5bf39b37dcc39128e472b61ed31660989
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-08-06 23:04:53 +0200

    refactor s3 storage and setting

--- src/App.js
+++ src/App.js
@@ -10,32 +10,65 @@ import HomePage from './components/Home/HomePage';
 import awsExports from './aws-exports';
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 
-Amplify.configure(awsExports)
+
+
+import { Authenticator, View, Image, useTheme, Text  } from '@aws-amplify/ui-react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@aws-amplify/ui-react/styles.css';
+import Contacts from './components/contacts/Contacts';
+
+
+
+Amplify.configure(awsExports);
+
 function App() {
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Contacts App"
+            src="/img/logo.png"
+          />
+        </View>
+      );
+    },
+    Footer() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; 2022 Cumulus Cycles 
+          </Text>
+        </View>
+      );
+    },
+  };
+
   return (
-      <Authenticator>
-        {({ signOut, user }) => (
-         
+    <Authenticator loginMechanisms={['email']} components={components}>
+    {({ signOut, user }) => (
+      <div>
+        <SiteNav logOut={signOut} />
+        <Routes>
+          <Route path='*' element={<HomePage />} />
+          <Route path='/' exact={true} element={<HomePage />} />
+          <Route path='/contacts' element={<Contacts />} />
 
-    <div>
-     <SiteNav logOut = {signOut} />
-     <Routes>
-      <Route path='*' element= {< HomePage />} />
-      <Route path='/' exact= {true} element= {< HomePage />} />
-
-
-
-
-     </Routes>
-      <SiteFooter />
-    </div>
-      )}
+        </Routes>
+        <SiteFooter />
+      </div>
+    )}
     </Authenticator>
-
-        )}
+  );
+}
 
 export default App;
