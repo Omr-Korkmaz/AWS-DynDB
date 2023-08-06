19c2f7cde0facafc3241bdebdefd11bdcb19e9e7
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-08-06 22:17:44 +0200

    add auth and amplify user and cognito user group

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awscontactsaa9d0b82": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
