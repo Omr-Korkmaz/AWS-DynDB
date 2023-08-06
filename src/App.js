357b128870a8b39da97fbae6f0f85b1b7f849bc4
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-08-06 22:49:40 +0200

    fix bug about logout to go back homepage

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import SiteFooter from './components/comman/SiteFooter';
 import SiteNav from './components/comman/SiteNav';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/Home/HomePage';
-import LoginPage from './components/Auth/LoginPage';
-import RegisterPage from './components/Auth/RegisterPage';
+
 import awsExports from './aws-exports';
 import { Amplify } from 'aws-amplify';
 
@@ -20,27 +19,23 @@ function App() {
   return (
       <Authenticator>
         {({ signOut, user }) => (
-          <main>
-            <h1>Hello {user.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
+         
 
     <div>
-     <SiteNav />
+     <SiteNav logOut = {signOut} />
      <Routes>
       <Route path='*' element= {< HomePage />} />
       <Route path='/' exact= {true} element= {< HomePage />} />
 
-      <Route path='/login' element= {< LoginPage />} />
-      <Route path='/register' element= {< RegisterPage />} />
+
 
 
      </Routes>
       <SiteFooter />
     </div>
+      )}
     </Authenticator>
-  );
-}
+
+        )}
 
 export default App;
