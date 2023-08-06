dcb3b1e8533fe8e6c0dba62c76fc1e553b382be6
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-08-06 22:31:21 +0200

    add amplify UI and refactor

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import SiteFooter from './components/comman/SiteFooter';
-
 import SiteNav from './components/comman/SiteNav';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/Home/HomePage';
 import LoginPage from './components/Auth/LoginPage';
 import RegisterPage from './components/Auth/RegisterPage';
+import awsExports from './aws-exports';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 
+Amplify.configure(awsExports)
 function App() {
   return (
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+
     <div>
      <SiteNav />
      <Routes>
@@ -26,6 +39,7 @@ function App() {
      </Routes>
       <SiteFooter />
     </div>
+    </Authenticator>
   );
 }
 
